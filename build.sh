7718d9a13a8452cc3fb03bd83ac0a38ac63b6bf4
Mermelada-de-Moras <enigmapacman086@gmail.com>
2024-09-26 19:31:16 -0400

    actualiza build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
+
 set -o errexit
 
 #poetry install
 
-python manage.py collectstatic --no-input
-
 pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 
 python manage.py migrate
