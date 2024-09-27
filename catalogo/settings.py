0950a2b8196ef8e28a3efa91ce0ecab8bbb35819
Mermelada-de-Moras <enigmapacman086@gmail.com>
2024-09-27 18:18:58 -0400

    database

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'catalogo.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/catalogo',
         conn_max_age=600
     )
 }
